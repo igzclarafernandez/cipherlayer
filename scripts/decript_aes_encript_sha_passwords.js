8c66ea7873b67b6a5636a32e6951a4fc34fbdf79
clarafernandez <clara.fernandez@intelygenz.com>
2017-12-01 12:36:02 +0100

    update script users-pass

--- scripts/decript_aes_encript_sha_passwords.js
+++ scripts/decript_aes_encript_sha_passwords.js
@@ -11,28 +11,55 @@ const config = require('../config');
 const cryptoMng = crypto(config.password);
 
 if (!module.parent) { // Run as CLI command exec
+
 	async.series([
 		// Start cipherLayer components (mongodb, redis...)
 		userDao.connect,
 
 		function drop(done) {
-			userDao.getAllUsers(function(err, users){
-				users.forEach(function (user) {
-					userDao.getAllUserFields(user.username, function (err, result) {
-						let decriptPass =  cryptoMng.decryptAES(result.password);
-						cryptoMng.encrypt(decriptPass, function (encryptedPwd){
-							if (!encryptedPwd){
-								log.error({err: 'no se ha devuelto ninguna contraseña para encriptar para el usuario '+ user.username , result: ''});
-							}
-							userDao.updateField(user._id, 'password', encryptedPwd, function (err, result) {
-								log.error({err: err, result: ' el usauio ' + user.username +'ha termindo con resultado ' + result});
-							});
-						});
+
+
+
+			userDao.getAllUsers( function(err, users){
+				if(err) return done();
+
+				function updatePass(user, encryptedPwd, cbk){
+					userDao.updateField(user._id, 'password', encryptedPwd, function (err, result) {
+						if (result == 0){
+							log.error({result: ' no se ha actualizado nada para el usauio ' + user._id + " " + user.username });
+						} else {
+							log.info({result: 'usuario actualizado ' + user._id + " " + user.username });
+						}
+						return cbk()
 					});
-				});
-			});
+				}
+
+				function processUser(err, user) {
+					if(!user || err) {
+						log.info({result: 'no hay mas usuarios ++++++++++++++++++++', err });
+						return done();
+					}
+
+					let decriptPass = cryptoMng.decryptAES(user.password);
+					log.error({err: 'contraseña del usuario  ' + user.username + " es " + decriptPass});
+					cryptoMng.encrypt(decriptPass, function (encryptedPwd) {
+						if (!encryptedPwd) {
+							log.error({err: 'no se ha devuelto ninguna contraseña para encriptar para el usuario ' + user.username});
+						}
+						log.error({err: 'la contraseña encriptada del usuario ' + user.username + " es " + encryptedPwd});
+						updatePass(user, encryptedPwd, function(){
+							users.nextObject(processUser);
+						})
+					});
+				}
+
+				users.nextObject(processUser);
+			})
+
 		},
+
 		userDao.disconnect
+
 	], err => {
 		if (err) {
 			console.error(err);
